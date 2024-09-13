dbae4d5e34e540d0fa3bd90c55949b815d6618c5
Bismay Kumar <44846718+Codsssworth@users.noreply.github.com>
2024-09-13 15:27:41 +0530

    Add files via upload

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    pguser: "postgres",       
+    pghost: "localhost",       
+    pgDatabase: "postgres",    
+    pgpass: "root",            
+    pgPort: 5432,
+    JWT_SECRET: 'your_secret_key_here',
+    DATABASE_URL : "uri"                
+};
+
+
